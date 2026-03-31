59b4f86c2d9118296f98e16203c000e6ce0902cf
HHLhhx <923511708@qq.com>
2026-03-31 16:24:38 +0800

    feat: 重构实现逻辑，提高性能

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.nju"
-version = "1.2.0"
+version = "2.0.1"
 
 repositories {
     mavenCentral()
@@ -61,7 +61,7 @@ intellijPlatform {
 
     publishing {
         token.set(System.getenv("PUBLISH_TOKEN"))
-        channels.set(listOf("stable"))
+        channels.set(listOf("beta"))
     }
 }
 
